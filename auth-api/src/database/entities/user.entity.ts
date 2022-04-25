9a9a37fd96d789f1bfc49eb64f63efec8ea8b3e0
Kristian Hadzhikolev <balccbf@gmail.com>
2022-04-25 16:10:20 +0200

    auth-api, update typeorm config in app module, created user entity

---
+++ auth-api/src/database/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  role: string;
+}
