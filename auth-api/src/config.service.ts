3880b3b3487cc8a9e3724b9ed7235fbcadb10e17
Kristian Hadzhikolev <balccbf@gmail.com>
2022-04-25 16:05:23 +0200

    auth-api, setting up default controller and app starting

--- auth-api/src/config.service.ts
+++ auth-api/src/config.service.ts
@@ -1,50 +1,51 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config('../../.env');
 
 class ConfigService {
-    constructor(private env: { [k: string]: string | undefined }) {}
+  constructor(private env: { [k: string]: string | undefined }) {}
 
-    private getValue(key: string, throwOnMissing = true): string {
-        const value = this.env[key];
-        if (!value && throwOnMissing) {
-            throw new Error(`config error - missing env.${key}`);
-        }
-
-        return value;
+  private getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
     }
 
-    public ensureValues(keys: string[]): any {
-        keys.forEach(k => this.getValue(k, true));
-        return this;
-    }
+    return value;
+  }
 
-    public getTypeOrmConfig(): TypeOrmModuleOptions {
-        return {
-            name: 'default',
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            type: (this.getValue('AUTH_DB_TYPE') as any) || 'mysql',
-            port: parseInt(this.getValue('AUTH_DB_PORT')) || 3306,
-            host: this.getValue('AUTH_DB_HOST') || 'localhost',
-            username: this.getValue('USERS_DB_USERNAME') || 'root',
-            password: this.getValue('USERS_DB_PASSWORD') || '',
-            database: this.getValue('USERS_DB_NAME') || 'ausers',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,
-            cli: {
-                entitiesDir: 'src/database/entities',
-            },
-        };
-    }
+  public ensureValues(keys: string[]): any {
+    keys.forEach((k) => this.getValue(k, true));
+    return this;
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      name: 'default',
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      type: (this.getValue('AUTH_DB_TYPE') as any) || 'mysql',
+      port: parseInt(this.getValue('AUTH_DB_PORT')) || 3306,
+      host: this.getValue('AUTH_DB_HOST') || 'localhost',
+      username: this.getValue('USERS_DB_USERNAME') || 'root',
+      password: this.getValue('USERS_DB_PASSWORD') || '',
+      database: this.getValue('USERS_DB_NAME') || 'ausers',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      cli: {
+        entitiesDir: 'src/database/entities',
+      },
+    };
+  }
 }
 
 const configService = new ConfigService(process.env).ensureValues([
-    'AUTH_DB_TYPE',
-    'AUTH_DB_HOST',
-    'AUTH_DB_PORT',
-    'AUTH_DB_USERNAME',
-    'AUTH_DB_PASSWORD',
-    'AUTH_DB_NAME',
+  'AUTH_DB_TYPE',
+  'AUTH_DB_HOST',
+  'AUTH_DB_PORT',
+  'AUTH_DB_USERNAME',
+  'AUTH_DB_PASSWORD',
+  'AUTH_DB_NAME',
 ]);
 
 export { configService };
