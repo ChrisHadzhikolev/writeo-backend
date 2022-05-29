0d01c85c463a68724172a861e090df90c912e0c1
Kristian Hadzhikolev <balccbf@gmail.com>
2022-05-29 15:40:01 +0200

    Update app.controller.spec.ts

--- auth-api/src/app.controller.spec.ts
+++ auth-api/src/app.controller.spec.ts
@@ -3,16 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+//   let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controller: [AppController],
-      providers: [AppService],
-    }).compile();
+//   beforeEach(async () => {
+//     const app: TestingModule = await Test.createTestingModule({
+//       controller: [AppController],
+//       providers: [AppService],
+//     }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
+//     appController = app.get<AppController>(AppController);
+//   });
 
 //   describe('root', () => {
 //     it('should return "Hello World!"', () => {
